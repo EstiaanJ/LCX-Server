a5245cb8118d52af0715222955b0bab3a7a01bec
EstiaanJ <xero416@gmail.com>
2016-07-11 18:34:49 +0800

    Started rewriting the Server class.

--- src/lcx/LCX.java
+++ src/lcx/LCX.java
@@ -7,6 +7,8 @@ package lcx;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 /**
@@ -15,16 +17,19 @@ import java.net.ServerSocket;
  */
 public class LCX extends Thread
     {
+    public static final Logger systemLog = Logger.getLogger( LCX.class.getName() );
     public static DatabaseInterface databaseIF;
     private final static int PORT = 2388;
     private ServerSocket serverSocket;
     private final static int STD_TIMEOUT = 5;
 
+    
+            
     public LCX() throws IOException
         {
 
         }
-
+    
     @Override
     public void run()
         {
@@ -42,14 +47,15 @@ public class LCX extends Thread
             System.err.println("Could not listen on port " + String.valueOf(PORT));
             System.exit(-1);
             }
-        System.out.println("Waiting for a client...");
+        systemLog.log(Level.FINE, "Ready for clients.");
         while(true)
             {
-            ClientAcceptor clientAcceptor;
+            systemLog.log(Level.INFO, "Waiting for client...");
+            Server server;
             try
                 {
-                clientAcceptor = new ClientAcceptor(serverSocket.accept());
-                Thread thread = new Thread(clientAcceptor);
+                server = new Server(serverSocket.accept());
+                Thread thread = new Thread(server);
                 thread.start();
                 }
             catch (IOException e)
--- src/lcx/Server.java
+++ src/lcx/Server.java
@@ -5,239 +5,147 @@
  */
 package lcx;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.math.BigInteger;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
-import java.security.SecureRandom;
-import java.util.logging.FileHandler;
 import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
-
-import shared.Commands;
-
+import shared.CommonMessages;
 /**
  *
  * @author Estiaan Janse Van Rensburg <https://github.com/EstiaanJ>
  */
-public class Server
+public class Server implements Runnable 
+//A thread and object of this class only ever deals with one client program instance (if the user closes the client program, the thread it used will expire)
     {
-    private Socket client;
-    private UserAccount user;
-    private Logger userTransLog;
-    private static Logger systemLog;
-    private FileHandler fh;
-    private String serverUSID = "unset";
-    private DataInputStream in;
-    private DataOutputStream out;
-    private final String firstCommand;
-    boolean running = true;
+    private static final String SERVER_VERSION = "0.2";
+    private BufferedReader in;
+    private PrintWriter out;
+    private final Socket clientSocket;
+    private ClientInstance clientInstance;
     
-
-    
-    public Server(Socket inClient,DataInputStream inDIS,DataOutputStream inDOS,String inFirstCommand)
+    public Server(Socket inClientSocket)
         {
-        firstCommand = inFirstCommand;
-        if(firstCommand.equals("New Session"))
+        clientInstance = new ClientInstance(SERVER_VERSION);
+        clientSocket = inClientSocket;
+        try
             {
-            
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            out = new PrintWriter(clientSocket.getOutputStream());
+            }
+        catch(SocketTimeoutException ste)
+                {
+                LCX.systemLog.log(Level.SEVERE, "Socket timed out while recieving data streams. {0}", ste.toString());
+                }
+        catch(IOException e)
+                {
+                LCX.systemLog.log(Level.SEVERE, "IOException while recieving data streams. {0}", e.toString());
+                }
+        LCX.systemLog.log(Level.FINE, "Client connected: {0}", clientSocket.getLocalSocketAddress());
+        }
+    
+    
+    @Override
+    public void run()
+        {
+        LCX.systemLog.log(Level.FINE, "Determining client version.");
+        determineClientVersion();
+        LCX.systemLog.log(Level.FINE, "Client version is: {0}", clientInstance.getClientVersion());
+        
+        LCX.systemLog.log(Level.FINE,"Sending Server version to client: {0}", clientInstance.getServerVersion());
+        sendCommand(CommonMessages.VERSION_MESSAGE_START + clientInstance.getServerVersion());
+        
+        LCX.systemLog.log(Level.FINE,"Server is ready to listen for client requests.");
+
+        LCX.systemLog.log(Level.FINE,"Waiting for request from client...");
+        waitForCommands();
+        closeConnection();
+        }
+    
+    private void determineClientVersion()
+        {
+        sendCommand(CommonMessages.VERSION_REQUEST.msg());
+
+        String version = recieveMessage();
+        
+        if(version.contains(CommonMessages.VERSION_MESSAGE_START.msg()))
+            {
+            //Set the client version to the recieved version, but remove the "VERSION" part at the front.
+            LCX.systemLog.log(Level.FINER,"Client Version Recieved");
+            clientInstance.setClientVersion(version.replace(CommonMessages.VERSION_MESSAGE_START.msg(),""));
             }
         else
             {
-            System.err.println("First Command was not 'New Session'");
-            System.exit(-1); //Need a better way to handle this first command thing, but I don't really see how it won't be the right command, unless it was sent to the original client worker by mistake.
+            LCX.systemLog.log(Level.WARNING, "Failed to establish client version, client sent {0}", version);
+            clientInstance.setClientVersion(CommonMessages.DEFAUL_VERSION.msg()); 
             }
-        client = inClient;
-        in = inDIS;
-        out = inDOS;
-        serverUSID = genUSID();
-        System.out.println("Client connected: " + client.getLocalSocketAddress() + " USID: " + serverUSID);
         }
-  
-    private void sendCommand(DataOutputStream out, Commands com) throws IOException 
+    
+    private void waitForCommands()
         {
-        out.writeUTF(com.msg());
+        String recievedMessage;
+        CommonMessages command;
+        
+        while(clientInstance.isActive() && (recievedMessage = recieveMessage()) != null )
+            {
+            command = CommonMessages.fromString(recievedMessage);
+            if(command == null)
+                {
+                LCX.systemLog.log(Level.SEVERE,"An invalid command was recieved from client: {0}", command);
+                break;
+                }
+            switch(command)
+                {
+            case LOGIN_REQUEST:
+                
+                break;
+                }
+            }
         }
     
-    private String readString(DataInputStream in) throws IOException
-    {
-        return in.readUTF();
-    }
-    
-    private void sendString(DataOutputStream out, String sendstr) throws IOException
-    {
-        out.writeUTF(sendstr);
-    }
-    
-
-    public void run()
+    private void closeConnection()
         {
         try
             {
-            sendCommand(out, Commands.NEW_SESSION_ACKNOWLEDGE);
-            System.out.println("Client: New Session");
-            while(running)
-                {
-                waitForCommand(in,out);
-                }
+            in.close();
             }
-        catch (SocketTimeoutException s)
+        catch(IOException e)
             {
-            System.out.println("Socket timed out!");
+            LCX.systemLog.log(Level.SEVERE,"Cound't close input stream");
             }
-        catch (IOException e)
-            {
-            e.printStackTrace();
-            }
+        out.close();
         }
     
-    private void waitForCommand(DataInputStream in,DataOutputStream out) throws IOException 
+    private boolean containsWord(String text,String word)
         {
-        if(running)
-            {
-             System.out.println("Waiting for command from client...");
-             String inMessage = readString(in);
-            Commands command = Commands.fromString(inMessage);
-            switch (command)
-                {
-            case NEW_SESSION_REQUEST:
-                //The stuff in here was moved to "run" since it will always be called first.
-                System.out.println("Getting to this println statment, in case NEW_SESSION_REQUEST: on switch(command), should be impossible...");
-                break;
-            case NEW_USID_REQUEST:
-                sendString(out,genUSID());
-                System.out.println("Client: New USID");
-                break;
-            case LOGIN_REQUEST:
-                loginRequested(in,out);
-                break;
-            case UPDATE_REQUEST:
-                System.out.println("Client: Update");
-                sendString(out,LCX.databaseIF.readName(user.getUserNumber(),serverUSID));
-                System.out.println("Client says " + readString(in));
-                sendString(out,LCX.databaseIF.readLatinumString(user.getUserNumber(),serverUSID));
-                System.out.println("Client says " + readString(in));
-                sendString(out,LCX.databaseIF.readTransactionLog(user.getUserNumber(),serverUSID));
-                break; 
-            case NEW_USER_REQUEST:
-                System.out.println("Client: New User");
-                sendCommand(out, Commands.NEW_USER_ACKNOWLEDGE);
-                String newUserAccNum = readString(in);
-                sendCommand(out, Commands.RECEIPT_ACCOUNT_NUMBER);
-                String newName = readString(in);
-                sendCommand(out, Commands.RECEIPT_ACCOUNT_NAME);
-                String newPass = readString(in);
-                sendCommand(out, Commands.RECEIPT_ACCOUNT_PASSWORD);
-                if(LCX.databaseIF.createNewAccount(newUserAccNum,newName,newPass))
-                    {
-                    System.out.println("Created Account Succesfully");
-                    }
-                else
-                    {
-                    System.err.println("Failed to create account");
-                    }
-                break;
-            case NEW_ACCOUNT_NUMBER_REQUEST:
-                System.out.println("Client: New Account Number");
-                sendString(out, LCX.databaseIF.newAccountNumber());
-                break;
-            case NEW_TRANSFER_REQUEST:
-                System.out.println("Client: Transfer");
-                sendCommand(out,Commands.NEW_TRANSFER_AWAITING_RECEIPIENT);
-                String transferTo = readString(in);
-                sendCommand(out,Commands.NEW_TRANSFER_AWAITING_AMOUNT);
-                String transferAmount = readString(in);
-                userTransLog.log(Level.INFO, "[TRANSFER OUT]: Transfering: {0} Transfering to: {1}", new Object[]{transferAmount, transferTo});
-                LCX.databaseIF.transfer(user.getUserNumber(),transferTo,transferAmount);
-                userTransLog.log(Level.INFO, "[TRANSFER REPORT]: Transfer Complete. Account now has: {0}", LCX.databaseIF.readLatinumString(user.getUserNumber(),genUSID()));
-                sendCommand(out,Commands.RECEIPT_TRANSFER_COMPLETE);
-                break;
-            case CONNECTION_CLOSE_REQUEST:
-                System.out.println("Client: Close");
-                sendCommand(out, Commands.CONNECTION_CLOSE_ACKNOWLEDGE);
-                running = false;
-                this.fh.close();
-                in.close();
-                out.close(); 
-                break;
-            default:
-                System.err.println("[Error]: default case reached in command switch, an illegal command was probably sent by the client.");
-                System.out.println("Attempting to send error to client...");
-                sendString(out, "SERVER ERROR");
-                if(readString(in).equals("Error Report Request"))
-                    {
-                    sendString(out,inMessage);
-                    System.out.println("Client says " + readString(in));
-                    sendString(out, "[Server Error]: The server reported an illegal command was sent, that command was: " + command);
-                    System.out.println("The error was succesfully sent to the client.");
-                    }
-                else
-                    {
-                    System.err.println("[Error]: Could not report illegal command to client! There is either a flaw in the client's command recieving structure, or the client is stuck in a loop and cannot read commands.");
-                    System.out.println("The server cannot recover this thread from this situation. Closing connection.");
-                    client.close();
-                    }
-                }
-            
-            }
-        else
-            {
-            System.out.println("This thread is supposed to stop. If this println statment keeps appearing there is an issue");
-            }
-        //System.out.println("Exited wait for command");
+        boolean doesContain = false;
+        doesContain = word.contains(word);
+        return doesContain;
         }
     
-    private void loginRequested(DataInputStream in,DataOutputStream out) throws IOException
+    private String recieveMessage()
         {
-        System.out.println("Client: Login Request");
-        System.out.println("Exiting command structure to recieve login data");
-        sendCommand(out,Commands.LOGIN_AWAITING_ACCOUNT_NUMBER);
-        String inAccountNum = readString(in);
-        sendCommand(out,Commands.LOGIN_AWAITING_PASSWORD);
-        String inPassword = readString(in);
-        boolean validLogin = LCX.databaseIF.login(inAccountNum,inPassword,serverUSID);
-        if(validLogin)
+        String message = CommonMessages.NO_MESSAGE.msg();
+        
+        try
             {
-            sendCommand(out,Commands.LOGIN_PREPARE_FOR_DETAILS);
-            user = new UserAccount (inAccountNum,inPassword);
-            System.out.println("Login was succesful, sending user data to client");
-            sendString(out,LCX.databaseIF.readName(user.getUserNumber(),serverUSID));
-            System.out.println(readString(in));
-            sendString(out,LCX.databaseIF.readLatinumString(user.getUserNumber(),serverUSID));
-            System.out.println(readString(in));
-            sendString(out,LCX.databaseIF.readTransactionLog(user.getUserNumber(),serverUSID));
-            System.out.println(readString(in));
-            
-            this.userTransLog = Logger.getLogger(user.getUserNumber() + "_log");
-            try
-                {
-                fh = new FileHandler(user.getUserNumber() + "_Transaction_log.txt");
-                userTransLog.addHandler(fh);
-                SimpleFormatter formatter = new SimpleFormatter();
-                fh.setFormatter(formatter);
-                }
-            catch(IOException e)
-                {
-                e.printStackTrace();  
-                }
-            
-            sendCommand(out, Commands.LOGIN_COMPLETE_RECEIPT);
-            System.out.println("Returning to command structure");
+            message = in.readLine();
+            LCX.systemLog.log(Level.FINEST,"Read message from in stream buffer: {0}",message);
             }
-        else
+        catch(IOException e)
             {
-            sendCommand(out, Commands.LOGIN_FAIL_RECEIPT);
-            System.out.println("[Login Error]: Login was unsuccesful");
-            System.out.println("Returning to command structure");
+            LCX.systemLog.log(Level.WARNING,"Failed to read message from client");
             }
+        return message;
         }
     
-    public static String genUSID()
+    private void sendCommand(String inCommand)
         {
-        SecureRandom random = new SecureRandom();
-        return new BigInteger(130, random).toString(32);
+        LCX.systemLog.log(Level.FINEST,"Sending message to client: {0}",inCommand);
+        out.println(inCommand);
         }
     }
